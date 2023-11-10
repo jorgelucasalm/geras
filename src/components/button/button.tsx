4a4fd5b17c65aba405fb9b6a88bee4ba7c79532f
Jorge Lucas <jorge.lucasalm@gmail.com>
2023-11-09 21:04:03 -0300

    feat: add onClick props in Button

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -3,8 +3,13 @@ import { Content } from "./button-style";
 interface ButtonProps {
   children: any;
   category?: string;
+  onClick?: () => any;
 }
 
-export function Button({ children, category }: ButtonProps) {
-  return <Content category={category}>{children}</Content>;
+export function Button({ children, category, onClick }: ButtonProps) {
+  return (
+    <Content onClick={onClick} category={category}>
+      {children}
+    </Content>
+  );
 }
