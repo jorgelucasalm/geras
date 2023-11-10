a90b852e6a68d2440f9ed7fc51389cfcb33028ed
Jorge Lucas <jorge.lucasalm@gmail.com>
2023-11-09 21:04:33 -0300

    refactor: change footer to receive children

--- src/components/footer/footer-style.tsx
+++ src/components/footer/footer-style.tsx
@@ -6,21 +6,15 @@ import styled from "styled-components";
 
 export const Footer = styled.footer`
   display: flex;
-  flex-direction: row;
   align-items: center;
   justify-content: center;
   min-height: 4rem;
+  gap: 1rem;
 
   box-shadow: 0px 4px 4px 0px #00000019;
   background: var(--blue-100);
   width: 100%;
   padding: 0.5rem 0;
-
-  div {
-    display: flex;
-    gap: 1rem;
-    max-width: 22.5rem;
-  }
 
   img {
     width: 1.5rem;
--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,13 +1,8 @@
 import { Footer as FooterStyle } from "./footer-style";
-import { Button } from "@components/button/button";
+import { PropsWithChildren } from "react";
 
-export function Footer() {
-  return (
-    <FooterStyle>
-      <div>
-        <Button category="secundary">Anterior</Button>
-        <Button>Proximo</Button>
-      </div>
-    </FooterStyle>
-  );
+interface Props extends PropsWithChildren {}
+
+export function Footer({ children }: Props) {
+  return <FooterStyle>{children}</FooterStyle>;
 }
