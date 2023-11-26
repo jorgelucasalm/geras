b9212149bc49e97ec91e60ce71b1e533dc215a98
Jorge Lucas <jorge.lucasalm@gmail.com>
2023-11-26 15:26:54 -0300

    refactor: add dinamic icon and search options

--- src/components/select/select.tsx
+++ src/components/select/select.tsx
@@ -1,22 +1,35 @@
-import { Select } from "antd";
+import { Select, SelectProps } from "antd";
 import { Main } from "./select-style";
+import { ReactNode } from "react";
 
-export function MainSelect() {
+interface Props {
+  options: { value: string; label: string }[];
+  onChange?: (e: string) => any;
+  isSearch?: boolean;
+  placeholder?: string | undefined;
+  prefixIcon?: ReactNode;
+}
+
+export function MainSelect({ prefixIcon, options, isSearch, onChange, ...rest }: Props) {
   return (
     <Main>
+      {prefixIcon && <div className="prefix-icon-wrapper">{prefixIcon}</div>}
       <Select
-        placeholder={"Gênero"}
+        showSearch={isSearch}
         style={{
           width: 296,
           height: 48,
           borderRadius: 100,
         }}
-        onChange={() => {}}
-        options={[
-          { value: "m", label: "Masculino" },
-          { value: "f", label: "Feminino" },
-          { value: "o", label: "Outro" },
-        ]}
+        onChange={onChange}
+        options={options}
+        filterOption={(input, option) =>
+          (option?.label.toLocaleLowerCase() ?? "").includes(input.toLocaleLowerCase())
+        }
+        filterSort={(optionA, optionB) =>
+          (optionA?.label ?? "").toLowerCase().localeCompare((optionB?.label ?? "").toLowerCase())
+        }
+        {...rest}
       />
     </Main>
   );
