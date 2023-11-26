796c1789403fe0827cf62e13c9fc47939cd20b9e
Jorge Lucas <jorge.lucasalm@gmail.com>
2023-11-26 15:27:30 -0300

    feat: add logic to next step

--- src/components/donwload-page/step-one/index.tsx
+++ src/components/donwload-page/step-one/index.tsx
@@ -4,15 +4,29 @@ import { useLocation, useNavigate } from "react-router-dom";
 import { Div } from "./style";
 import SearchInput from "@components/search-input/search-input";
 import { CiSearch } from "react-icons/ci";
+import { MainSelect } from "@components/select/select";
 
 export default function StepOne() {
   const location = useLocation();
   const pathname = location.pathname.slice(1).split("/");
-  const [messageText, App] = pathname;
+  const [messageText, App, stepText, stepValueText] = pathname;
+  const navigate = useNavigate();
 
   return (
     <Div>
-      <SearchInput width={296} height={48} placeholder="Buscar" prefix={<CiSearch />} />
+      <MainSelect
+        placeholder={"Busca"}
+        isSearch={true}
+        options={[
+          { value: "uber", label: "Uber" },
+          { value: "whatsapp", label: "Whatsapp" },
+          { value: "ifood", label: "Ifood" },
+        ]}
+        prefixIcon={<CiSearch size={24} />}
+        onChange={(e: string) => {
+          e == App && navigate(`/${messageText}/${App}/${stepText}/${Number(stepValueText) + 1}`);
+        }}
+      />
       <h3> Você procura por: {App}</h3>
       <TextCard>Aqui vão ser exibidos os resultados da sua busca.</TextCard>
     </Div>
